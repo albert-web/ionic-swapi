aa58a32ffd536994ec58543d5ce7af8af4c3380a
albertmensah <DMensah@madisoncollege.edu>
2020-11-17 11:24:29 -0600

    Tool dependency on HttpClient

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,3 +1,5 @@
+
+import {HttpClient} from '@angular/common/http'
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,5 @@ import { Injectable } from '@angular/core';
 })
 export class SwapiService {
 
-  constructor() { }
+  constructor(private httpSvc: HttpClient) { }
 }
